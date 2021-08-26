b02af62137a7286f267f8d805e6a91ca3f16600f
Dung Le (Ryan) <51136599+dungzle@users.noreply.github.com>
2021-08-26 16:17:46 -0700

    Update move_file.py

--- move_file.py
+++ move_file.py
@@ -21,10 +21,10 @@ def get_latest_file(folder_path):
 
 # Move recent added files from folders in source_dir to target_dir
 if __name__ == "__main__":
-	source_dir = ['C:\\Users\\vietd\\Desktop\\Fortinet\\a',
-				  'C:\\Users\\vietd\\Desktop\\Fortinet\\b',
-				  'C:\\Users\\vietd\\Desktop\\Fortinet\\c']			  
-	target_dir = 'C:\\Users\\vietd\\Desktop\\Fortinet\\target'
+	source_dir = ['C:\\Users\\vietd\\Desktop\\a',
+				  'C:\\Users\\vietd\\Desktop\\b',
+				  'C:\\Users\\vietd\\Desktop\\c']			  
+	target_dir = 'C:\\Users\\vietd\\Desktop\\target'
 		
 	latest_files = [get_latest_file(folder) for folder in source_dir]
 	for folder_path,files in zip(source_dir,latest_files):
